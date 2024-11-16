d865e7e26b8359344c0896876debaeae1acf61fd
Pascal Schuppli <pschuppli@phar.ch>
2024-11-16 16:50:38 +0100

    Removed unnecessary code from Demo1

--- demo1_helloworld.py
+++ demo1_helloworld.py
@@ -5,18 +5,13 @@ WIDTH = 700
 HEIGHT = 750
 
 # Wenn ALWAYS_REDRAW True ist, dann wird das ganze Fenster in jedem Frame neu gezeichnet. Ansonsten musst du
-# request_redraw() aufrufen, wenn ein ein Update der Grafik benötigt wird.
+# request_redraw() aufrufen, wenn ein ein Update der Grafik benötigt wird. Da der Zustand der Grafik hier
+# nicht wegen Benutzerinput oder verstreichender Zeit von Frame zu Frame ändert, setzen wir ALWAYS_REDRAW auf
+# False, um CPU-Ressourcen zu sparen.
 ALWAYS_REDRAW = False
 
-# Legt die maximale Framerate fest. Default ist 60.
-#MAX_FPS = 120
-
-# Legt fest, ob das Fenster im Fullscreen-Modus geöffnet wird. Default ist False.
-#FULLSCREEN = True
-
-# Legt fest, ob die Grösse des Fensters vom Benutzer verändert werden kann. Default ist False.
+# Legt fest, ob die Grösse des Fensters vom Benutzer verändert werden kann - in diesem Fall ja.
 RESIZABLE = True
-
 
 
 image = None
@@ -40,18 +35,7 @@ def draw():
     # Zeichnet das geladene Baum-Bild auf den Bildschirm
     if image:
         draw_surface(image, (w/2-64,400))
-        
 
-def update(dt):
-    # Wird so oft wie möglich aufgerufen, aber maximal MAX_FPS mal pro Sekunde. dt gibt die Anzahl Millisekunden seit
-    # dem letzten Aufruf an.    
-    pass
-
-def input(event):
-    # Wird aufgerufen, wenn ein Ereignis (z.B. ein Mausklick oder ein Tastendruck) vorliegt
-    
-    if event.type == MOUSEBUTTONDOWN and event.button == 1:
-        pass
 
 def ready():
     # Wird aufgerufen, wenn das Grafik-Framework bereit ist, unmittelbar vor dem Start der Event Loop.    
@@ -66,13 +50,7 @@ def ready():
     except FileNotFoundError:
         print("Could not find image, going on without it...")
 
-def resized(new_width, new_height):
-    # Wird aufgerufen, wenn die Grösse des Grafikfensters verändert wird
-    print("resized window to new dimensions", new_width, new_height)
 
-def exit():
-    # Wird aufgerufen, bevor die Applikation beendet wird.
-    print("Finishing now...")
 
 
 # Startet das Grafikprogramm.
